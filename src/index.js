6e0f9c7b5ec53b9b5f996e76d0650dd718cb57d1
NhlanhlaZiks <125467381+NhlanhlaZiks@users.noreply.github.com>
2024-06-28 18:24:41 +0200

    injected more weather data

--- src/index.js
+++ src/index.js
@@ -1,9 +1,40 @@
 function refreshWeather(response) {
   let temperatureElement = document.querySelector("#temperature");
-  let cityElement = document.querySelector("#city");
-
   let temperature = response.data.temperature.current;
+  let cityElement = document.querySelector("#city");
+  let descriptionElement = document.querySelector("#description");
+  let humidityElement = document.querySelector("#humidity");
+  let windSpeedElement = document.querySelector("#wind-speed");
+  let timeElement = document.querySelector("#time");
+  let date = new Date(response.data.time * 1000);
+
+  cityElement.innerHTML = response.data.city;
+  descriptionElement.innerHTML = response.data.condition.description;
+  humidityElement.innerHTML = `${response.data.temperature.humidity}%`;
+  windSpeedElement.innerHTML = `${response.data.wind.speed}Km/h`;
+  timeElement.innerHTML = formatDate(date);
   temperatureElement.innerHTML = Math.round(temperature);
+}
+
+function formatDate(date) {
+  let hours = date.getHours();
+  let minutes = date.getMinutes();
+  let days = [
+    "sunday",
+    "monday",
+    "tuesday",
+    "wednsday",
+    "thurday",
+    "friday",
+    "saturday",
+  ];
+
+  let day = days[date.getDay()];
+  if (minutes < 10) {
+    minutes = `0${minutes}`;
+  }
+
+  return `${day} ${hours} :${minutes}`;
 }
 
 function searchCity(city) {
@@ -15,7 +46,7 @@ function searchCity(city) {
 function handleSearchSubmit(event) {
   event.preventDefault();
   let searchInput = document.querySelector("#search-form-input");
-  let cityElement = document.querySelector("#city");
+
   searchCity(searchInput.value);
 }
 let searchFormElement = document.querySelector("#search-form");
